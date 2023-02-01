6aa0c07433fe6269e9d19acf16c06e3e23b1165a
Mayurp375 <mayurpolojwar1234@gmail.com>
2023-02-01 16:05:23 +0530

    welcome to State census

---
+++ Main.java
@@ -0,0 +1,7 @@
+package org.example.Day29PracticeProblem;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to StateCensusAnalyser");
+    }
+}
